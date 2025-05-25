916f7508386f3859ac33793a83075b182a2a818b
Nkosikenny <kennybhekumuzi@gmail.com>
2025-05-25 09:46:28 +0200

    Refactor bubbleSort function for improved clarity and add example usage with custom comparator

--- Excercises/bubble-sort/sort.js
+++ Excercises/bubble-sort/sort.js
@@ -21,25 +21,58 @@
  *       > sorting can be smallest to largest or largest to smallest(ascending or descending)
  */
 
- // Simple sorting with default comparator
-// function bubbleSort(arr, comparator) {
-//   if(arr.length===0) return [];
-//   let noSwaps;
-//   for (let i = arr.length; i > 0; i--) {
-//     noSwaps = true;
-//     for (let j = 0; j < i - 1; j++) {
-//       if (arr[j] > arr[j + 1]) {
-//         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
-//         noSwaps = false;
-//       }
-//     }
-//     if (noSwaps) break;
-//   }
-//   return arr;
+// Simple sorting with default comparator
+function bubbleSort(arr, comparator) {
+    if(arr.length===0){
+        return [];
+    }
+    
+  if (typeof comparator !== "function") {
+     comparator=(a, b)=> {
+      if (a < b) {
+        return -1;
+      } else if (a > b) {
+        return 1;
+      } else {
+        return 0;
+      }
+    }
+  }
+ 
+  let noSwaps;
+  for (let i = arr.length; i > 0; i--) {
+    noSwaps = true;
+    for (let j = 0; j < i - 1; j++) {
+      if (comparator(arr[j], arr[j+1])>0) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        noSwaps = false;
+      }
+    }
+    if (noSwaps) break;
+  }
+  return arr;
+}
+
+const kittyData = [
+    { name: "LilBub", age: 7 },
+    { name: "Garfield", age: 40 },
+    { name: "Blue", age: 1 }
+];
+
+const ageComp=(a,b)=>b.age-a.age;
+
+console.log(bubbleSort(kittyData,ageComp));
+
+// console.log(bubbleSort([5, 4, -3, 2, 1, 0]));
+
+
+// function strComp(a, b) {
+//   if (a < b) { return -1;}
+//   else if (a > b) { return 1;}
+//   return 0;
 // }
 
-// console.log(bubbleSort([5,4,-3,2,1,0]));
+// console.log(bubbleSort(['z','c','b','e'],strComp));
 
 
-// sorting with strings
 
